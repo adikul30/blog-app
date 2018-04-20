966e3e97df4316ee6fc43bffdf26185d8fb54bf4
adikul30 <adi.kul30@gmail.com>
2018-04-20 20:45:53 +0530

    C&R out of CRUD are working

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+class Blog(models.Model):
+	"""docstring for Blog"""
+	# def __init__(self, arg):
+	# 	super(Blog, self).__init__()
+	# 	self.arg = arg
+	blog_title = models.CharField(max_length=100)
+	blog_content = models.TextField(max_length=800)
+	blog_author = models.CharField(max_length=50)
+	pub_date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.blog_title
+		
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Blog
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your views here.
+def index(request):
+	latest_blogs = Blog.objects.order_by('-pub_date')[:5]
+	context = {'latest_blogs': latest_blogs}
+	return render(request, 'blog/index.html', context)
+
+def detail(request, blog_id):
+	blog = get_object_or_404(Blog, pk = blog_id)
+	return render(request, 'blog/detail.html', {'blog' : blog,})
+
+def new(request):
+	if request.method == "GET":
+		return render(request,'blog/new.html',{})
+	elif request.method == 'POST':
+		title = request.POST.get("title","")
+		content = request.POST.get("content","")
+		author = request.POST.get("author","")
+		new_blog = Blog(blog_title = title, blog_content = content, blog_author = author, pub_date=timezone.now())
+		new_blog.save()
+		print(new_blog.id)
+		return HttpResponseRedirect(reverse('blog:detail', args = (new_blog.id,)))
